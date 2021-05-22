0880e386091a02224dc556fedff36ee29cd2ab49
heyputnam <heyputnam@gmail.com>
2021-05-22 10:58:34 -0400

    added google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: "my tutorial site",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    {
+      resolve: 'gatsby-plugin-google-fonts',
+      options: {
+        fonts: [
+          `Bebas Neue`
+        ],
+        display: "swap",
+      }
+    }
+  ],
 };
