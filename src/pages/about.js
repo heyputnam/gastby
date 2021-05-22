8d04decb1908320568d47ead7624142425fc37a6
heyputnam <heyputnam@gmail.com>
2021-05-21 21:11:34 -0400

    basic pages

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return(
+        <main>
+            <h1>Here's so cool facts about me</h1>
+            <ul>
+                <li>im terrified of the dentist and because of that ive been in jaw pain for a month and cant open my mouth all the way</li>
+                <li>I have two cat sons, Ned and Red. They are the goodest boys</li>
+                <li>My boyfriend owns an auto detailing shop and I have my own office in the back that I had to paint multiple time multiple colors because I cant decide</li>
+            </ul>
+
+        </main>
+    )
+}
+
+
+export default AboutPage
